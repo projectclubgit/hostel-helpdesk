5262d0df74f1d1d3e57e9ffe2a9129069cfc4eab
arunsri7 <arun.i1997@gmail.com>
2017-04-20 23:36:12 +0530

    login

---
+++ js/config.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDGJkgqPmaTbhfG3r_9d-XZ1IMsD87HJlw",
+  authDomain: "help-desk-286a6.firebaseapp.com",
+  databaseURL: "https://help-desk-286a6.firebaseio.com",
+  projectId: "help-desk-286a6",
+  storageBucket: "help-desk-286a6.appspot.com",
+  messagingSenderId: "521048382801"
+};
+
+firebase.initializeApp(config);
