94e6fddbb826a6b6490d2a372f2a30f25480aeb5
Arjun Mahishi <arjun.mahishi@gmail.com>
2017-07-12 01:17:59 +0530

    Faculty notify cards functional

--- js/notify.js
+++ js/notify.js
@@ -1,45 +1,52 @@
-// <div class="demo-card-square mdl-card mdl-shadow--2dp">
-//     <div class="mdl-card__title mdl-card--expand">
-//       <h2 class="mdl-card__title-text">Arun Iyer</h2>
-//     </div>
-//     <div class="mdl-card__supporting-text"> Reason for Outpass </div>
-//     <div class="mdl-card__actions mdl-card--border">
-//       <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-//         View 
-//       </button>
-//     </div>
-// </div>
-
-
+var cardsJSON = {};
 
 firebase.database().ref('/outpass').on("child_added", snap => {
 
 	console.log("in");
 
+	cardsJSON[snap.val().regno] = snap.val();
+
 	const outerDiv = document.createElement('div');
-	const titleDiv = document.createElement('div');
-	const title = document.createElement('h2');
-	const reasonDiv = document.createElement('div');
-	const buttonDiv = document.createElement('div');
-	const button = document.createElement('button');
-
-	title.innerHTML = snap.val().name;
-	reasonDiv.innerHTML = snap.val().reason_for_leave;
-	button.innerHTML = "View full details"
-
-	outerDiv.className = "demo-card-square mdl-card mdl-shadow--2dp";
-	titleDiv.className = "mdl-card__title mdl-card--expand";
-	title.className = "mdl-card__title-text";
-	reasonDiv.className = "mdl-card__supporting-text";
-	buttonDiv.className = "mdl-card__actions mdl-card--border";
-	button.className = "mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect";
-
-	titleDiv.appendChild(title);
-	buttonDiv.appendChild(button);
-	outerDiv.appendChild(titleDiv);
-	outerDiv.appendChild(reasonDiv);
-	outerDiv.appendChild(buttonDiv);
+	outerDiv.innerHTML = `
+		<div class="demo-card-square mdl-card mdl-shadow--2dp">
+			<div class="mdl-card__title mdl-card--expand">
+			<h2 class="mdl-card__title-text">${snap.val().name}</h2>
+			</div>
+			<div class="mdl-card__supporting-text">${snap.val().reason_for_leave}</div>
+			<div class="mdl-card__actions mdl-card--border">
+			<button onclick="showMoreDetails('${snap.val().regno}')" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+				View full details 
+			</button>
+			</div>
+		</div>
+	`;
 
 	document.getElementById("list-of-outpass").appendChild(outerDiv);
 
 });
+
+function showMoreDetails(regno){
+
+	const data = cardsJSON[regno];
+
+	let moreDetails = `
+		<h3>${data.name}</h3>
+		<h4>${data.regno}</h4>
+		<h4><b>Section: </b>${data.section}</h4> 
+		<p><b>Reason for leave: </b>${data.reason_for_leave}</p>
+		<p>
+			<span><b>Date out: </b>${data.date_out}</span>
+			<span class="right-float"><b>Date in: </b>${data.date_in}</span>
+		</p>
+		<p>
+			<span><b>Time out: </b>${data.time_out}</span>
+			<span class="right-float"><b>Time in: </b>${data.time_in}</span>
+		</p>
+		<p><b>Student Contact number: </b> ${data.contact_student}</p>
+		<p><b>Parent Contact number: </b> ${data.contact_home}</p>
+	`;
+
+	document.getElementById("more-details").innerHTML = moreDetails;
+
+	$("#show-full-form").modal("show");
+}
